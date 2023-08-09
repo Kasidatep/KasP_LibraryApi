9c4a18e35b186ff7bb62f3e08e724795288fbefc
kasidatep <bank@kasidate.me>
2023-08-09 21:58:52 +0700

    ADD post updated, delete: endpont

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ const getPosts = async () => {
 
         return rows;
     } catch (error) {
-        throw new Error('Error fetching posts');
+        throw new Error('Error fetching posts')
     }
 }
 
@@ -46,7 +46,7 @@ const getPostsUser = async () => {
         JOIN users u ON p.userId = u.id
       `)
         connection.release()
-        
+
         const formattedPostsPromises = rows.map(formatPostRow)
         const formattedPosts = await Promise.all(formattedPostsPromises)
         return formattedPosts
@@ -58,7 +58,7 @@ const getPostsUser = async () => {
 
 const getPostsById = async (postid) => {
     try {
-        const connection = await db.getConnection();
+        const connection = await db.getConnection()
         const [rows] = await connection.execute(`
         SELECT p.*, u.*, p.id as pid, u.id as uid
         FROM posts p
@@ -71,7 +71,7 @@ const getPostsById = async (postid) => {
         const formattedPosts = await Promise.all(formattedPostPromise)
         return formattedPosts
     } catch (error) {
-        throw new Error('Error fetching posts: ' + error.message);
+        throw new Error('Error fetching posts: ' + error.message)
     }
 }
 
@@ -92,21 +92,77 @@ const createPost = async (newPost) => {
             newPost.userId,
             convertDate.convertToMySQLDatetime(newPost.postDate),
         ]
-
-        console.log(values)
-        console.log("A1")
-        const result = await connection.query(query,values)
-        console.log("A2") 
+        const [result] = await connection.query(query, values)
         connection.release()
-        return { message: 'Post created successfully', postId: result.insertId }
-    } catch(error) {
-        console.error(error);
-        return { message: 'Post cannot be created.', error: error.message }
+
+        if (result.affectedRows === 1) {
+            return { message: 'Post created successfully', postId: result.insertId }
+        } else {
+            throw new Error('Failed to create post');
+        }
+    } catch (error) {
+        return { message: 'Error creating post', error: error.message }
+    }
+}
+
+const updatePost = async (updatedPost, id) => {
+    try {
+        const connection = await db.getConnection();
+
+        const query = `
+            UPDATE posts
+            SET title = ?, description = ?, img = ?, visible = ?, userId = ?, postDate = ?
+            WHERE id = ?
+        `
+
+        const values = [
+            updatedPost.title,
+            updatedPost.description,
+            updatedPost.img,
+            updatedPost.visible,
+            updatedPost.userId,
+            convertDate.convertToMySQLDatetime(updatedPost.postDate),
+            id
+        ]
+
+        const result = await connection.query(query, values)
+        connection.release()
+
+        if (result.affectedRows === 0) {
+            return { message: 'No matching post found for update', postId: id }
+        }
+
+        return { message: 'Post updated successfully', postId: id }
+    } catch (error) {
+        console.error(error)
+        return { message: 'Post could not be updated.', error: error.message }
     }
 }
 
 
+const deletePostsById = async (postId) => {
+    try {
+        const connection = await db.getConnection()
+
+        const query = `
+            DELETE FROM posts
+            WHERE id = ?
+        `
+
+        const [result] = await connection.execute(query, [postId])
+        connection.release();
+
+        if (result.affectedRows === 0) {
+            return { message: 'No matching post found for deletion', postId }
+        }
+
+        return { message: 'Post deleted successfully', postId }
+    } catch (error) {
+        throw new Error('Error deleting post: ' + error.message)
+    }
+}
+
 
 module.exports = {
-    getPosts, getPostsUser, getPostsById, createPost,
+    getPosts, getPostsUser, getPostsById, createPost, updatePost, deletePostsById,
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,48 +1,44 @@
-const express = require('express');
-const postModel = require('../models/post');
-
-const router = express.Router();
+const express = require('express')
+const postModel = require('../models/post')
+const router = express.Router()
 
 router.get('/posts', async (req, res) => {
   try {
-    const posts = await postModel.getPosts();
-    res.json(posts);
+    const posts = await postModel.getPosts()
+    res.json(posts)
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching posts' });
+    res.status(500).json({ error: 'Error fetching posts' })
   }
-});
+})
 
 router.get('/posts/all', async (req, res) => {
   try {
-    const postsWithUser = await postModel.getPostsUser();
-    res.json(postsWithUser);
+    const postsWithUser = await postModel.getPostsUser()
+    res.json(postsWithUser)
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching posts with user data' });
+    res.status(500).json({ error: 'Error fetching posts with user data' })
   }
-});
+})
 
 router.get('/posts/:id', async (req, res) => {
   try {
-    const postsById = await postModel.getPostsById(req.params.id);
+    const postsById = await postModel.getPostsById(req.params.id)
     if (postsById.length > 0) {
-      const singlePost = postsById[0];
+      const singlePost = postsById[0]
       res.json(singlePost);
     } else {
-      // Handle case where no post was found
-      res.status(404).json({ error: 'Post not found' });
+      res.status(404).json({ error: 'Post not found' })
     }
   } catch (error) {
-    res.status(500).json({ error: `Error fetching posts ${req.params.id} with user data | ${error.message}` });
+    res.status(500).json({ error: `Error fetching posts ${req.params.id} with user data | ${error.message}` })
   }
-});
+})
 
 
-router.post('/posts', (req, res) => {
+router.post('/posts', async (req, res) => {
 
   try {
-    const { title, description, img, visible, userId, postDate } = req.body;
-
-    // Add validation if needed
+    const { title, description, img, visible, userId, postDate } = req.body
     if (!req.body.title || !req.body.description || !req.body.visible.toString().match(/^[1-2]{1}$/g) || !req.body.userId) {
       res.status(400)
       res.json({ message: "Bad Request" })
@@ -55,17 +51,50 @@ router.post('/posts', (req, res) => {
       visible,
       userId,
       postDate,
-    };
+    }
 
-    const result = postModel.createPost(newPost); // Assuming you have a createPost function in your model
-    res.status(200).json(result); // Respond with the created post
+    const result = await postModel.createPost(newPost)
+    res.json(result)
   } catch (error) {
-    console.error('Error creating post:', error);
-    res.status(500).json({ error: 'An error occurred while creating the post.' });
+    console.error('Error creating post:', error)
+    res.status(500).json({ error: 'An error occurred while creating the post.' })
   }
-}
-)
+})
 
+router.put('/posts/:id', async (req, res) => {
+  try {
+    const { title, description, img, visible, userId, postDate } = req.body
+    if (!req.body.title || !req.body.description || !req.body.visible.toString().match(/^[1-2]{1}$/g) || !req.body.userId) {
+      res.status(400)
+      res.json({ message: "Bad Request" })
+    }
+    const updatePost = {
+      id:req.params.id,
+      title,
+      description,
+      img,
+      visible,
+      userId,
+      postDate,
+    }
+
+    const result = await postModel.updatePost(updatePost, req.params.id)
+    res.json(result)
+  } catch (error) {
+    console.error('Error update post:', error)
+    res.status(500).json({ error: 'An error occurred while updating the post.' })
+  }
+})
+
+router.delete('/posts/:id', async (req, res) => {
+  try{
+    const result = await postModel.deletePostsById(req.params.id)
+    res.json(result)
+  } catch (error) {
+    console.error('Error delete post:', error)
+    res.status(500).json({ error: 'An error occurred while deleting the post.' })
+  }
+})
 
 module.exports = router
 
